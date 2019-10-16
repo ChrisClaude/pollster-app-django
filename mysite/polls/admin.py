74c707b8b24562280a4dd5214216abc829fe98cc
ChClaude <christ.tchambila@gmail.com>
2019-10-16 19:04:47 +0200

    django tuto 7 part

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Choice, Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # The first element of each tuple in fieldsets is the title of the fieldset
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
